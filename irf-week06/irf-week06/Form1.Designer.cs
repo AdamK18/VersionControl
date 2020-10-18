c35a4a1c8a6543dd1a7a0c18e300bbce1f0c70f6
k.Adam <kloczl.adam@gmail.com>
2020-10-18 23:30:40 +0200

    currencies done

--- irf-week06/irf-week06/Form1.Designer.cs
+++ irf-week06/irf-week06/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.startDatePicker = new System.Windows.Forms.DateTimePicker();
             this.endDatePicker = new System.Windows.Forms.DateTimePicker();
@@ -40,17 +40,17 @@
             // 
             // chartRateData
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chartRateData.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chartRateData.Legends.Add(legend2);
-            this.chartRateData.Location = new System.Drawing.Point(292, 37);
+            chartArea1.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend1);
+            this.chartRateData.Location = new System.Drawing.Point(2, 37);
             this.chartRateData.Name = "chartRateData";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chartRateData.Series.Add(series2);
-            this.chartRateData.Size = new System.Drawing.Size(496, 401);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartRateData.Series.Add(series1);
+            this.chartRateData.Size = new System.Drawing.Size(786, 401);
             this.chartRateData.TabIndex = 0;
             this.chartRateData.Text = "chart1";
             // 
--- irf-week06/irf-week06/Form1.cs
+++ irf-week06/irf-week06/Form1.cs
@@ -17,53 +17,75 @@ namespace irf_week06
     public partial class Form1 : Form
     {
         private BindingList<RateData> Rates = new BindingList<RateData>();
-        BindingList<String> currencies;
-        ComboBox box;
-        DateTimePicker startDate;
-        DateTimePicker endDate;
+        BindingList<string> currencies = new BindingList<string>();
 
 
         public Form1()
         {
             InitializeComponent();
-            currencies = getCurrencies();
+            getCurrencies();
             startDatePicker.Value = DateTime.Today.AddDays(-30);
-            endDate.Value = DateTime.Now;
+            endDatePicker.Value = DateTime.Now;
             RefreshData();
         }
 
         private void RefreshData()
         {
-            box = currencyBox;
-            startDate = startDatePicker;
-            endDate = endDatePicker;
             Rates.Clear();
-            var result = getSoapClient(currencyBox.SelectedItem.ToString(), startDatePicker.Value.ToString(), endDatePicker.Value.ToString());
+            parseXML(getSoapClient(currencyBox.SelectedItem.ToString(), startDatePicker.Value.ToString(), endDatePicker.Value.ToString()));
             DataGridView view = new DataGridView();
             view.DataSource = Rates;
-            currencyBox.DataSource = currencies;
-            parseXML(result);
             showDataOnChart();
         }
 
-        private BindingList<String> getCurrencies()
+        private void getCurrencies()
+        {
+            var response = new MNBArfolyamServiceSoapClient().GetCurrencies(new GetCurrenciesRequestBody());
+            var result = response.GetCurrenciesResult;
+            XmlDocument curXml = new XmlDocument();
+            curXml.LoadXml(result);
+            var childElement = (XmlElement)curXml.ChildNodes[0].ChildNodes[0];
+            foreach (XmlElement element in childElement)
+            {
+                currencies.Add(element.InnerText);
+            }
+            currencyBox.DataSource = currencies;
+        }
+
+        private void parseXML(String result)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                if (childElement == null) continue;
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+                Rates.Add(rate);
+            }
+        }
+
+        private static string getSoapClient(String currency, String startDate, String endDate)
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
-
             var request = new GetExchangeRatesRequestBody()
             {
                 currencyNames = currency,
                 startDate = startDate,
                 endDate = endDate
             };
-
-            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
-            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
             var response = mnbService.GetExchangeRates(request);
-
-            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
-            // Ezért a result változó valójában string típusú.
             var result = response.GetExchangeRatesResult;
+            return result;
         }
 
         private void showDataOnChart()
@@ -83,61 +105,6 @@ namespace irf_week06
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
-        }
-
-        private void parseXML(String result)
-        {
-            XmlDocument xml = new XmlDocument();
-            xml.LoadXml(result);
-
-            // Végigmegünk a dokumentum fő elemének gyermekein
-            foreach (XmlElement element in xml.DocumentElement)
-            {
-                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
-                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
-                var rate = new RateData();
-
-                // Dátum
-                rate.Date = DateTime.Parse(element.GetAttribute("date"));
-
-                // Valuta
-                var childElement = (XmlElement)element.ChildNodes[0];
-                rate.Currency = childElement.GetAttribute("curr");
-
-                // Érték
-                var unit = decimal.Parse(childElement.GetAttribute("unit"));
-                var value = decimal.Parse(childElement.InnerText);
-                if (unit != 0)
-                {
-                    rate.Value = value / unit;
-                }
-                Rates.Add(rate);
-            }
-        }
-
-        private static string getSoapClient(String currency, String startDate, String endDate)
-        {
-            // A változó deklarációk jobb oldalán a "var" egy dinamikus változó típus.
-            // A "var" változó az első értékadás pillanatában a kapott érték típusát veszi fel, és később nem változtatható.
-            // Jelen példa első sora tehát ekvivalens azzal, ha a "var" helyélre a MNBArfolyamServiceSoapClient-t írjuk.
-            // Ebben a formában azonban olvashatóbb a kód, és változtatás esetén elég egy helyen átírni az osztály típusát.
-            var mnbService = new MNBArfolyamServiceSoapClient();
-           
-            var request = new GetExchangeRatesRequestBody()
-            {
-                currencyNames = currency,
-                startDate = startDate,
-                endDate = endDate
-            };
-
-            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
-            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
-            var response = mnbService.GetExchangeRates(request);
-
-            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
-            // Ezért a result változó valójában string típusú.
-            var result = response.GetExchangeRatesResult;
-            return result;
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
