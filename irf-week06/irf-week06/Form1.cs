c5de16426e3b0c4c0aef40d1c6260ea628c63c47
k.Adam <kloczl.adam@gmail.com>
2020-10-18 13:15:40 +0200

    forgot to commmmiiiiiiit

--- irf-week06/irf-week06/Form1.cs
+++ irf-week06/irf-week06/Form1.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using irf_week06.Entities;
+using irf_week06.MnbServiceReference;
 
 namespace irf_week06
 {
     public partial class Form1 : Form
     {
+        private BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            getSoapClient();
+            DataGridView view = new DataGridView();
+            view.DataSource = Rates;
+            XmlDocument xml = new XmlDocument(); 
+        }
+
+        private static void getSoapClient()
+        {
+            // A változó deklarációk jobb oldalán a "var" egy dinamikus változó típus.
+            // A "var" változó az első értékadás pillanatában a kapott érték típusát veszi fel, és később nem változtatható.
+            // Jelen példa első sora tehát ekvivalens azzal, ha a "var" helyélre a MNBArfolyamServiceSoapClient-t írjuk.
+            // Ebben a formában azonban olvashatóbb a kód, és változtatás esetén elég egy helyen átírni az osztály típusát.
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
