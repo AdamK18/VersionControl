d19db7ca87680c4bec5a7ab780d516b703c96b4b
k.Adam <kloczl.adam@gmail.com>
2020-11-22 23:46:20 +0100

    6 feladat

--- UnitTestExample/UnitTestExampleTest/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExampleTest/AccountControllerTestFixture.cs
@@ -10,7 +10,11 @@ namespace NUnit.Framework
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
